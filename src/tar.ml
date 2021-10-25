addc0b5d63ea7e3470507cb739db8ee366025e57
Arseniy Alekseyev <aalekseyev@janestreet.com>
2021-10-25 19:06:08 +0100

    v0.15~preview.125.10+20

--- src/tar.ml
+++ src/tar.ml
@@ -18,15 +18,14 @@ let create ~working_dir ~files tar =
   (* The tar should be deterministic if possible, to avoid causing spurious rebuilds.
      This option isn't supported on mac os, it seems, so we use this conditionally. *)
   let deterministic_archive_flag = [ "--mtime"; "2017-01-01" ] in
-  let deterministic_env = [ ("GZIP", "-n") ] in
   Process.run ~working_dir ~prog:"tar"
     ~args:(deterministic_archive_flag @ [ "--version" ]) ()
   >>| Result.is_ok
   >>= fun support_mtime ->
   let args =
     (if support_mtime then deterministic_archive_flag else [])
-    @ [ "-czf"; tar ]
+    @ [ "-I"; "gzip -n"; "-cf"; tar ]
     @ files
   in
-  Shell.run ~env:(`Extend deterministic_env) ~working_dir "tar" args
+  Shell.run ~working_dir "tar" args
 ;;
